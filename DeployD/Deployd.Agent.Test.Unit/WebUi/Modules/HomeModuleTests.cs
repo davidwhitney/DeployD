86f87a1b216d0293dec0bf025cc2e475d50260b9
David Whitney <david@davidwhitney.co.uk>
2012-02-29 22:11:59 +0000

    stubbed out start of webui testing.

---
+++ DeployD/Deployd.Agent.Test.Unit/WebUi/Modules/HomeModuleTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Deployd.Agent.WebUi;
+using NUnit.Framework;
+using Nancy;
+using Nancy.Testing;
+
+namespace Deployd.Agent.Test.Unit.WebUi.Modules
+{
+    [TestFixture]
+    public class HomeModuleTests
+    {
+        private Browser _browser;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _browser = new Browser(new NancyConventionsBootstrapper());
+        }
+
+        [Test]
+        public void Get_Homepage_ReturnsMarkup()
+        {
+            var result = _browser.Get("/", with => with.HttpRequest());
+
+            Assert.That(result.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        }
+    }
+}
