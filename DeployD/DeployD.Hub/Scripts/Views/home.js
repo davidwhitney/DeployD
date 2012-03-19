8ab48c1f26e17a4950609d8cfcda018de429fa96
andrewmyhre <andrew@justgiving.com>
2012-03-19 14:08:10 +0000

    implemented a draft hub page prototype

---
+++ DeployD/DeployD.Hub/Scripts/Views/home.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    $('#deployDialog').hide();
+    $('.cancel').click(function () {
+        $(this).parent().hide();
+    });
+
+    $('.package').click(function () {
+        $('#deployDialog').show();
+        $('#packageIdSelect').val($(this).text());
+    });
+});
