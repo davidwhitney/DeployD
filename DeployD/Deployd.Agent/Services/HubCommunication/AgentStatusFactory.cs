21111220eac39492107afecc6ea8fc35500c635b
andrewmyhre <andrew@justgiving.com>
2012-08-22 12:45:14 +0100

    update to hopefully avoid 'collection has been modified' error when building agent status report

--- DeployD/Deployd.Agent/Services/HubCommunication/AgentStatusFactory.cs
+++ DeployD/Deployd.Agent/Services/HubCommunication/AgentStatusFactory.cs
@@ -15,24 +15,34 @@ namespace Deployd.Agent.Services.HubCommunication
     {
         public static AgentStatusReport BuildStatus(IPackagesList availablePackages, ILocalPackageCache packageCache, IInstalledPackageArchive installCache, RunningInstallationTaskList runningTasks, IAgentSettingsManager settingsManager, ICurrentlyDownloadingList currentlyDownloadingList)
         {
+            // copying these collections to variables because sometimes they get modified while building the status report object
+            string[] updating = new string[currentlyDownloadingList != null ? currentlyDownloadingList.Count : 0];
+            if (currentlyDownloadingList != null) currentlyDownloadingList.CopyTo(updating, 0);
+
+            IPackage[] packages = new IPackage[availablePackages != null ? availablePackages.Count : 0];
+            if (availablePackages != null) availablePackages.CopyTo(packages, 0);
+
+            var watchedPackageList = availablePackages.GetWatched().ToList();
+            IPackage[] watchedPackages = new IPackage[watchedPackageList != null ? watchedPackageList.Count : 0];
+            if (watchedPackageList != null) watchedPackageList.CopyTo(watchedPackages);
+
+            InstallationTask[] tasks=new InstallationTask[runningTasks != null ? runningTasks.Count : 0];
+            if (runningTasks != null) runningTasks.CopyTo(tasks);
+
             var status= new AgentStatusReport
                        {
-                           packages = BuildPackageInformation(availablePackages.GetWatched(), installCache, runningTasks),
-                           currentTasks = runningTasks != null ?
-                                                    runningTasks.Select(t => new InstallTaskViewModel()
+                           packages = BuildPackageInformation(watchedPackages, installCache, tasks),
+                           currentTasks = tasks.Select(t => new InstallTaskViewModel()
                                                                        {
                                                                            Messages = t.ProgressReports.Select(pr => pr.Message).ToArray(),
                                                                            Status = Enum.GetName(typeof(TaskStatus), t.Task.Status),
                                                                            PackageId = t.PackageId,
                                                                            Version = t.Version,
                                                                            LastMessage = t.ProgressReports.Count > 0 ? t.ProgressReports.LastOrDefault().Message : ""
-                                                                       }).ToList()
-                                                                       : new List<InstallTaskViewModel>(),
-                           availableVersions = availablePackages != null ? 
-                                                availablePackages.Select(p => p.Version.ToString()).Distinct().OrderByDescending(s => s).ToList()
-                                                : new List<string>(),
+                                                                       }).ToList(),
+                           availableVersions = packages.Select(p => p.Version.ToString()).Distinct().OrderByDescending(s => s).ToList(),
                            environment = settingsManager.Settings.DeploymentEnvironment,
-                           updating = currentlyDownloadingList.Select(s=>s).ToList(),
+                           updating = updating.ToList(),
                            isUpdating = currentlyDownloadingList.Downloading
                        };
 
@@ -42,7 +52,7 @@ namespace Deployd.Agent.Services.HubCommunication
             return status;
         }
 
-        private static List<LocalPackageInformation> BuildPackageInformation(IEnumerable<IPackage> packages, IInstalledPackageArchive installCache, RunningInstallationTaskList runningTasks)
+        private static List<LocalPackageInformation> BuildPackageInformation(IEnumerable<IPackage> packages, IInstalledPackageArchive installCache, InstallationTask[] runningTasks)
         {
             List<LocalPackageInformation> packageInformations = new List<LocalPackageInformation>();
             var packagesById = packages.GroupBy(p=>p.Id);
